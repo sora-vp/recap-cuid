7b5bdd79e81d37fca5b0babc8674fcc482157bbf
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-12-29 10:38:40 +0700

    feat: menambahkan prettier dan react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,66 @@
+import { useEffect } from "react";
+import { motion } from "motion/react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <>Hey</>,
+  },
+  {
+    path: "/",
+    element: <>Halaman utama</>,
+  },
+  {
+    path: "recap",
+    element: <>download rekapan</>,
+  },
+  {
+    path: "settings",
+    element: <>pengaturan lah intinya</>,
+  },
+]);
+
 function App() {
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === ",") {
+        e.preventDefault();
+
+        router.navigate("/settings");
+      }
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+    };
+  }, []);
+
   return (
     <>
-    init
+      <RouterProvider router={router} />
+      <div className="absolute bottom-4 left-4">
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            duration: 0.8,
+          }}
+          className="font-sundanese select-none text-2xl"
+          onDoubleClick={() => location.reload()}
+        >
+          ᮞᮧᮛ
+          <p className="select-none text-xs">recap-cuid</p>
+        </motion.span>
+      </div>
+      <div className="absolute bottom-1 right-1">
+        <small className="font-sundanese font-mono">vALPHA-1</small>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
